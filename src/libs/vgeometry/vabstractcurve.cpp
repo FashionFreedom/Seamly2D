a60f757486ccf1f3165a675dab16f4c84451d241
Roman Telezhynskyi <kroluku@gmail.com>
2017-08-03 13:43:25 +0300

    Fixed issue #742. Valentina produces wrong seam allowance.

--- src/libs/vgeometry/vabstractcurve.cpp
+++ src/libs/vgeometry/vabstractcurve.cpp
@@ -116,7 +116,12 @@ QVector<QPointF> VAbstractCurve::FromBegin(const QVector<QPointF> &points, const
                 if (IsPointOnLineSegment(begin, points.at(i), points.at(i+1)))
                 {
                     theBegin = true;
-                    segment.append(begin);
+
+                    if (begin != points.at(i+1))
+                    {
+                        segment.append(begin);
+                    }
+
                     if (i == points.count()-2)
                     {
                          segment.append(points.at(i+1));
