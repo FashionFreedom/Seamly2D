5a1bd310038e236ba8de849d1e307fc820c9e755
Alexey_Shmuley <Alex@e_mail>
2018-01-05 08:37:46 +0300

    projectversion.cpp:change version

--- src/libs/vmisc/projectversion.cpp
+++ src/libs/vmisc/projectversion.cpp
@@ -67,8 +67,13 @@ extern const int MAJOR_VERSION = 0;
 extern const int MINOR_VERSION = 6;
 extern const int DEBUG_VERSION = 0;
 
-extern const QString APP_VERSION_STR(QStringLiteral("%1.%2.%3.%4a").arg(MAJOR_VERSION).arg(MINOR_VERSION)
-                                 .arg(DEBUG_VERSION).arg(LATEST_TAG_DISTANCE));
+
+extern const int SUPER_MINOR__VERSION = 0;
+extern const QString APP_VERSION_STR(QStringLiteral("%1.%2.%3.%4").arg(MAJOR_VERSION).arg(MINOR_VERSION)
+                                 .arg(DEBUG_VERSION).arg(SUPER_MINOR__VERSION));
+
+//extern const QString APP_VERSION_STR(QStringLiteral("%1.%2.%3.%4a").arg(MAJOR_VERSION).arg(MINOR_VERSION)
+//                                 .arg(DEBUG_VERSION).arg(LATEST_TAG_DISTANCE));
 
 //---------------------------------------------------------------------------------------------------------------------
 QString compilerString()
