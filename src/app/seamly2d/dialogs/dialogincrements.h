e843a907726f0010f162c48d6c05b17c44ca6e80
Ian <ianpatrickcunningham@gmail.com>
2020-09-19 13:18:52 -0500

    1. I renamed a bunch of variables and display names. (Tabs on the 'Variables' dialog had slightly incorrect wording - IE: "Lines angles" was renamed to 'Line angles". Also renamed corresponding variables/function names) 2. I created a private function on 'dialogIncrements.ccp/.h' that setup all the search/findPrevious/findNext functionality for every tab. In short:    - It creats a array of tuples, of size 7. Each tuple corresponding to a tab on the Tables of Variables dialog, and each containing a: (VTableSearch, QLineEdit, QToolButton, QToolButton)    - It iterates through the array, and hooks up the VTableSearch, QLineEdit, and QToolButtons accordingly 3. We now refresh all 7 VTableSearch objects when a new file is loaded (in functions 'UpdateTree' starting on line 546, and 'FullUpdateFromFile' starting on line 608) (Do we really need to do this? Can this window be open when a new file loads in?)
    One thing remaining, I'm not sure if I finished the UI file properly?? I had to 'Break Layout' to add the new widgets... I'm not sure how to 'unbreak' them??

--- src/app/seamly2d/dialogs/dialogincrements.h
+++ src/app/seamly2d/dialogs/dialogincrements.h
@@ -55,6 +55,7 @@
 #include "../vtools/dialogs/tools/dialogtool.h"
 #include "../xml/vpattern.h"
 #include "../vmisc/vtablesearch.h"
+#include <tuple>
 
 #include <QPair>
 
@@ -112,25 +113,33 @@ private:
 
     int                  formulaBaseHeight;
 
-    QSharedPointer<VTableSearch> search;
+    QSharedPointer<VTableSearch> searchIncrements;
+    QSharedPointer<VTableSearch> searchLines;
+    QSharedPointer<VTableSearch> searchLineAngles;
+    QSharedPointer<VTableSearch> searchCurveLengths;
+    QSharedPointer<VTableSearch> searchCurveControlPointLengths;
+    QSharedPointer<VTableSearch> searchCurveAngles;
+    QSharedPointer<VTableSearch> searchArcRadiuses;
 
     bool hasChanges;
 
     QVector<QPair<QString, QString>> renameList;
 
     template <typename T>
-    void                 FillTable(const QMap<QString, T> &varTable, QTableWidget *table);
+    void    FillTable(const QMap<QString, T> &varTable, QTableWidget *table);
 
-    void                 FillIncrements(bool freshCall = false);
-    void                 FillLengthsLines();
-    void                 FillLengthLinesAngles();
-    void                 FillLengthsCurves();
-    void                 FillCurvesCLengths();
-    void                 FillRadiusesArcs();
-    void                 FillAnglesCurves();
+    void    SetupTableSearch();
 
-    void                 ShowUnits();
-    void                 ShowHeaderUnits(QTableWidget *table, int column, const QString &unit);
+    void    FillIncrements(bool freshCall = false);
+    void    FillLineLengths();
+    void    FillLineAngles();
+    void    FillCurveLengths();
+    void    FillCurveControlPointLengths();
+    void    FillCurveAngles();
+    void    FillArcRadiuses();
+
+    void    ShowUnits();
+    void    ShowHeaderUnits(QTableWidget *table, int column, const QString &unit);
 
     void AddCell(QTableWidget *table, const QString &text, int row, int column, int aligment, bool ok = true);
 
