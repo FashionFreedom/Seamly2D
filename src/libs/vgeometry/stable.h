523dc2c31c70b68a8ef41023c6276a7837ad5230
Sue Spencer <susan.spencer@gmail.com>
2021-10-31 20:36:11 -0500

    fix pragma message \n

--- src/libs/vgeometry/stable.h
+++ src/libs/vgeometry/stable.h
@@ -53,7 +53,7 @@
 #define STABLE_H
 
 /* I like to include this pragma too, so the build log indicates if pre-compiled headers were in use. */
-#pragma message("Compiling precompiled headers for VGeometry library.\n")
+#pragma message("Compiling precompiled headers for VGeometry library.")
 
 /* Add C includes here */
 
