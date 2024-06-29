38c61e7c6a663d41e55a9f947f14361b7e5ccaa6
Onetchou <83351185+Onetchou@users.noreply.github.com>
2024-06-29 15:03:57 +0200

    feat: all the UI part of the image origin functionnality has been added, not the functionnality itself

--- src/libs/vmisc/def.cpp
+++ src/libs/vmisc/def.cpp
@@ -171,6 +171,7 @@ const QStringList labelTemplatePlaceholders = QStringList() << pl_size
 const QString cursorArrowOpenHand  = QStringLiteral("://cursor/cursor-arrow-openhand.png");
 const QString cursorArrowCloseHand = QStringLiteral("://cursor/cursor-arrow-closehand.png");
 const QString cursorResizeArrow = QStringLiteral("://cursor/arrow_resize_cursor.png");
+const QString cursorImageOrigin = QStringLiteral("://cursor/image_origin_cursor.png");
 
 // From documantation: If you use QStringLiteral you should avoid declaring the same literal in multiple places: This
 // furthermore blows up the binary sizes.
