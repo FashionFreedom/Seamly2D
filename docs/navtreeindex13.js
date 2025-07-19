245f79e077915b9e560a6350e583aef980554a5b
slspencer <susan.spencer@gmail.com>
2025-07-19 23:23:31 +0000

    Deploying to gh-pages from @ FashionFreedom/Seamly2D@ca735305ed6821ac6ebd3a68bac70cffa06c5100 🚀

--- docs/navtreeindex13.js
+++ docs/navtreeindex13.js
@@ -244,10 +244,10 @@ var NAVTREEINDEX13 =
 "d3/dbe/classDialogHeight.html#af079a35976c1756a7df9e194a7288d2a":[1,0,53,8],
 "d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html":[1,0,2,27],
 "d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html":[0,0,24,27],
-"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a1318f7b630a9749b414ef846978aeea2":[0,0,24,27,5],
 "d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a1318f7b630a9749b414ef846978aeea2":[1,0,2,27,5],
-"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28cf8d984dc9b6c38cdd1a5707e06748":[1,0,2,27,2],
+"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a1318f7b630a9749b414ef846978aeea2":[0,0,24,27,5],
 "d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28cf8d984dc9b6c38cdd1a5707e06748":[0,0,24,27,2],
-"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28d756adf17253a3ef933acb4112f673":[1,0,2,27,3],
-"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28d756adf17253a3ef933acb4112f673":[0,0,24,27,3]
+"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28cf8d984dc9b6c38cdd1a5707e06748":[1,0,2,27,2],
+"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28d756adf17253a3ef933acb4112f673":[0,0,24,27,3],
+"d3/dc0/classVPE_1_1VPropertyFormViewPrivate.html#a28d756adf17253a3ef933acb4112f673":[1,0,2,27,3]
 };
