0213454e9f37a654688df07d9ab2b925bc3069d6
DSCaskey <31944718+DSCaskey@users.noreply.github.com>
2025-12-31 22:36:21 +0000

    Deploying to gh-pages from @ FashionFreedom/Seamly2D@bdfd9ab9081ff1f2afa9c67790122eef676ee6fe 🚀

--- docs/search/enumvalues_8.js
+++ docs/search/enumvalues_8.js
@@ -49,7 +49,7 @@ var searchData=
   ['individual_46',['Individual',['../d4/da4/def_8h.html#a55ff83aa1faba5d2fcc34767a5502386a2dd4472245a696bc0b4b944db2a8b519',1,'def.h']]],
   ['information_47',['Information',['../dd/dc4/classUtils_1_1CheckableMessageBox.html#a3704f2110d227cfa444af687b6f086b4af76ff5d16bc77ee1bda33b7cb39d3882',1,'Utils::CheckableMessageBox']]],
   ['init_48',['Init',['../df/d51/qmudef_8cpp.html#a5d74787dedbc4e11c1ab15bf487e61f8a1ff76aa808bb8209a13fcdbaf330587e',1,'qmudef.cpp']]],
-  ['initial_5fpunctuation_49',['INITIAL_PUNCTUATION',['../dd/d02/classXMLUniCharacter.html#aef6cfc31fbb0bfda7917309f4802aef1a78e748dac9a8095e34c1b0d3cab93c96',1,'XMLUniCharacter']]],
+  ['initial_5fpunctuation_49',['INITIAL_PUNCTUATION',['../dd/d02/classXMLUniCharacter.html#a6f38eb37f0042c27a67f7179e132893ba78e748dac9a8095e34c1b0d3cab93c96',1,'XMLUniCharacter']]],
   ['inputdigit_50',['InputDigit',['../df/d51/qmudef_8cpp.html#a4c03a92b86f91e7e4ab59a095ad64226a3e5bdd94d86f35221254965300f96a80',1,'qmudef.cpp']]],
   ['inputdot_51',['InputDot',['../df/d51/qmudef_8cpp.html#a4c03a92b86f91e7e4ab59a095ad64226ae0975713475b374678e591a1f220157e',1,'qmudef.cpp']]],
   ['inputexp_52',['InputExp',['../df/d51/qmudef_8cpp.html#a4c03a92b86f91e7e4ab59a095ad64226a4d2312d27fa78117b16bc1ba15da28a9',1,'qmudef.cpp']]],
