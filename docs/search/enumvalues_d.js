85a8b89e84f7dbd0c52bed75eabe7217ec6638b6
csett86 <csett86_git@quicksands.de>
2026-01-01 08:41:06 +0000

    Deploying to gh-pages from @ FashionFreedom/Seamly2D@5d3af78e7e2a478a07eea354fc4b920056c4a475 🚀

--- docs/search/enumvalues_d.js
+++ docs/search/enumvalues_d.js
@@ -74,7 +74,7 @@ var searchData=
   ['noinfixop_71',['noINFIXOP',['../d8/daa/classqmu_1_1QmuParserTokenReader.html#ae8d5a1c2027478b943942506998cfcb6a6ac6f0bfa0e7b3b533d7ba0371167c10',1,'qmu::QmuParserTokenReader']]],
   ['nointersection_72',['NoIntersection',['../d9/d3d/classVPosition.html#a260c9e88536085034a0619920e00c651aa81313ddfa184bc1ecb63b5feae91e45',1,'VPosition']]],
   ['nomultiplevalues_73',['NoMultipleValues',['../d5/dc5/classXMLValid.html#ac30e7f5d9612c52f5f1208c83f090c86ae669fa512d99cb068afc80500329e2a8',1,'XMLValid']]],
-  ['non_5fspacing_5fmark_74',['NON_SPACING_MARK',['../dd/d02/classXMLUniCharacter.html#a1ab8b1de68bfcb71e9029d1ff4c1bfc4abc25853809a9c4e657b8a3d4e120ecb5',1,'XMLUniCharacter']]],
+  ['non_5fspacing_5fmark_74',['NON_SPACING_MARK',['../dd/d02/classXMLUniCharacter.html#a6f38eb37f0042c27a67f7179e132893babc25853809a9c4e657b8a3d4e120ecb5',1,'XMLUniCharacter']]],
   ['nonameglobalelement_75',['NoNameGlobalElement',['../d7/d8e/classXMLErrs.html#aefe9817ef5442905d86fd17cb0c35e47a6695f338434f8844a8d8c1eef64df4e9',1,'XMLErrs']]],
   ['nonamerefattgroup_76',['NoNameRefAttGroup',['../d7/d8e/classXMLErrs.html#aefe9817ef5442905d86fd17cb0c35e47a0f536a93dd525d9127bd1558b410aa1b',1,'XMLErrs']]],
   ['nonamerefattribute_77',['NoNameRefAttribute',['../d7/d8e/classXMLErrs.html#aefe9817ef5442905d86fd17cb0c35e47a7e4d53a15785a9318c54c9f666a85000',1,'XMLErrs']]],
